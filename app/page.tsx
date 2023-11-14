b97c3ca308c3594e977e98e73e3aa09b1f981172
emilfauzan <emilfauzan@gmail.com>
2023-11-14 15:40:23 +0700

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main className="overflow-hidden border m-5 p-5">
       <div className='grid grid-rows-1'>
-        <h2 className='flex justify-center text-lg text-blue-400 font-black border-4 rounded-md p-5  border-orange-400 m-2'>alo</h2>
+        <h2 className='flex justify-center text-lg text-blue-400 font-black border-4 rounded-md p-5 bg-white border-orange-400 m-2'>alo</h2>
       </div>
       <div className='grid grid-rows-1'>
-        <h2 className='flex justify-center text-lg text-orange-400 font-black border-4 rounded-md p-5 border-blue-400 m-2'>ola</h2>
+        <h2 className='flex justify-center text-lg text-orange-400 font-black border-4 rounded-md p-5 bg-white border-blue-400 m-2'>ola</h2>
       </div>
     </main>
   )
