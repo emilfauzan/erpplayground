1e64878822b85d0cb9cca024b014959e596869d8
Emil Fauzan <emilfauzan@gmail.com>
2023-11-20 10:44:49 +0700

    modified TimeAndDate.tsx importing React

--- components/common/timeAndDate/TimeAndDate.tsx
+++ components/common/timeAndDate/TimeAndDate.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function padTo2Digits(num: number) {
     return num.toString().padStart(2, "0");
