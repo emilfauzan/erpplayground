3f7517962a54aae5bfe1c12b0bfe705d80b7ad0b
Emil Fauzan <emilfauzan@gmail.com>
2023-11-28 15:09:12 +0700

    modified dropdownuser

--- components/navigation/Topbar/DropdownUser.tsx
+++ components/navigation/Topbar/DropdownUser.tsx
@@ -1,6 +1,14 @@
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
-import { Button } from "@material-tailwind/react";
+import {
+    Button,
+    Menu,
+    MenuHandler,
+    MenuList,
+    MenuItem,
+    Avatar,
+    Typography,
+} from "@material-tailwind/react";
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
 import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded';
@@ -41,15 +49,16 @@ const DropdownUser = () => {
 
     return (
         <>
-            <div className="relative">
-                <Link
-                    ref={trigger}
-                    onClick={() => setDropdownOpen(!dropdownOpen)}
-                    className="flex items-center gap-4"
-                    href={"#"}
-                >
+            <Menu>
+                <MenuHandler>
+                    {/* <Link
+                        ref={trigger}
+                        onClick={() => setDropdownOpen(!dropdownOpen)}
+                        className="flex items-center gap-4"
+                        href={"#"}
+                    > */}
                     <svg
-                        className="mr-5 hidden hover:text-orange-400 text-white sm:block duration-300 ease-in-out"
+                        className="mr-5 hidden hover:text-orange-400 text-white sm:block duration-300 ease-in-out cursor-pointer"
                         width={60}
                         height={45}
                         viewBox="0 0 12 8"
@@ -57,42 +66,74 @@ const DropdownUser = () => {
                     >
                         <AccountCircleRoundedIcon />
                     </svg>
-                </Link>
-                <div
-                    ref={dropdown}
-                    onFocus={() => setDropdownOpen(true)}
-                    onBlur={() => setDropdownOpen(false)}
-                    className={`absolute right-2 mt-5 w-62.5 flex-col rounded-md border border-stroke bg-blue-gray-800 text-white shadow-default dark:border-strokedark ${dropdownOpen === true ? "block" : "hidden"
-                        }`}
-                >
-                    <ul className="flex flex-col gap-5 border-b border-stroke px-6 py-5 dark:border-stroke">
-                        <h1 className="text-sm italic">
-                            <div className="border-b-2 pb-2"><PersonRoundedIcon className="mr-2" fontSize="small" /> Staff&apos;s Name </div><br />
-                            <div className="border-b-2 pb-2"><BadgeRoundedIcon className="mr-2" fontSize="small" /> Staff&apos;s ID </div><br />
-                            <div className="border-b-2 pb-2"><AssignmentIndRoundedIcon className="mr-2" fontSize="small" /> Staff&apos;s Position</div>
-                        </h1>
-                    </ul>
-                    <div className="text-center">
-                        <Button className="w-full gap-3.5 px-6 py-3 text-sm font-bolder duration-300 ease-in-out hover:bg-red-500 hover:border-red-500 lg:text-base rounded-sm">
-                            <Link
-                                href={"/"}
-                                className="flex items-center justify-center gap-3.5 text-sm font-bolder duration-300 ease-in-out "
-                            >
-                                <svg
-                                    className="fill-current"
-                                    width={22}
-                                    height={22}
-                                    viewBox="0 0 22 22"
-                                    fill="none"
+                    {/* </Link> */}
+                </MenuHandler>
+                <MenuList>
+
+                    <MenuItem className="flex items-center gap-2 cursor-default text-black">
+                        <PersonRoundedIcon className="mr-2" fontSize="small" />
+                        <Typography variant="small" className="font-medium">
+                            Staff&apos;s Name
+                        </Typography>
+                    </MenuItem>
+                    <MenuItem className="flex items-center gap-2 cursor-default text-black">
+                        <BadgeRoundedIcon className="mr-2" fontSize="small" />
+                        <Typography variant="small" className="font-medium">
+                            Staff&apos;s ID
+                        </Typography>
+                    </MenuItem>
+                    <MenuItem className="flex items-center gap-2 cursor-default text-black">
+                        <AssignmentIndRoundedIcon className="mr-2" fontSize="small" />
+                        <Typography variant="small" className="font-medium">
+                            Staff&apos;s Position
+                        </Typography>
+                    </MenuItem>
+
+                    <hr className="my-3" />
+                    <Button fullWidth variant="text" className="flex gap-2 py-2 border text-red-500 hover:bg-red-500 hover:text-white">
+                        <LogoutIcon className="mr-2" fontSize="small" />
+
+                        <Typography variant="small" className="font-medium">
+                            Logout
+                        </Typography>
+                    </Button>
+
+                    {/* <div
+                        ref={dropdown}
+                        onFocus={() => setDropdownOpen(true)}
+                        onBlur={() => setDropdownOpen(false)}
+                        className={`absolute right-2 mt-5 w-62.5 flex-col rounded-md border border-stroke bg-blue-gray-800 text-white shadow-default dark:border-strokedark ${dropdownOpen === true ? "block" : "hidden"
+                            }`}
+                    >
+                        <ul className="flex flex-col gap-5 border-b border-stroke px-6 py-5 dark:border-stroke">
+                            <h1 className="text-sm italic">
+                                <div className="border-b-2 pb-2"><PersonRoundedIcon className="mr-2" fontSize="small" /> Staff&apos;s Name </div><br />
+                                <div className="border-b-2 pb-2"><BadgeRoundedIcon className="mr-2" fontSize="small" /> Staff&apos;s ID </div><br />
+                                <div className="border-b-2 pb-2"><AssignmentIndRoundedIcon className="mr-2" fontSize="small" /> Staff&apos;s Position</div>
+                            </h1>
+                        </ul>
+                        <div className="text-center">
+                            <Button className="w-full gap-3.5 px-6 py-3 text-sm font-bolder duration-300 ease-in-out hover:bg-red-500 hover:border-red-500 lg:text-base rounded-sm">
+                                <Link
+                                    href={"/"}
+                                    className="flex items-center justify-center gap-3.5 text-sm font-bolder duration-300 ease-in-out "
                                 >
-                                    <LogoutIcon />
-                                </svg>
-                                Log Out
-                            </Link>
-                        </Button>
-                    </div>
-                </div>
-            </div>
+                                    <svg
+                                        className="fill-current"
+                                        width={22}
+                                        height={22}
+                                        viewBox="0 0 22 22"
+                                        fill="none"
+                                    >
+                                        <LogoutIcon />
+                                    </svg>
+                                    Log Out
+                                </Link>
+                            </Button>
+                        </div>
+                    </div> */}
+                </MenuList>
+            </Menu>
         </>
 
     );
