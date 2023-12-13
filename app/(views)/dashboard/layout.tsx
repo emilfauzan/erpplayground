8e1f6943217e7fab28561cacbd72babd237fd628
Emil Fauzan <emilfauzan@gmail.com>
2023-12-13 11:48:47 +0700

    modified footer stays at the bottom of the viewport

--- app/(views)/dashboard/layout.tsx
+++ app/(views)/dashboard/layout.tsx
@@ -34,7 +34,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
                             />
                             <main>
                                 <div
-                                    className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10"
+                                    className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10 min-h-fit	"
                                 >
                                     {children}
                                 </div>
