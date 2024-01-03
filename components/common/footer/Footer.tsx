4a050df994a22dffc61da15489d2e7ed0714e82b
Emil Fauzan <emilfauzan@gmail.com>
2024-01-03 10:44:29 +0700

    added animation for login page

--- components/common/footer/Footer.tsx
+++ components/common/footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="relative bottom-0 z-9 flex h-full  max-w-full rounded-none px-5 w-full py-6 bg-[#37474f] md:bg-[#F1F5F9] justify-center items-end">
             <Typography color="white" className="text-sm font-normal text-center md:text-black ">
-                Copyright &copy; 2023 <strong> NT Corp Online Analytics </strong>by JKO IT Development Team.
+                Copyright &copy; 2023 <strong> NT Corp Analytics </strong>by JKO IT Development Team.
             </Typography>
         </footer>
     )
--- pages/login/Login.tsx
+++ pages/login/Login.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import React from 'react'
-import { Visibility, VisibilityOff } from '@mui/icons-material';
+import React, { useEffect, useRef, useState } from 'react'
 import { Typography, Link, Grid, Tooltip, Divider } from '@mui/material'
 import Image from "next/image";
 import CompanyLogo from "@/public/images/companyLogo/companyLogo.png";
@@ -9,105 +8,109 @@ import { FormUsernamePassword } from '@/components/login/FormUsernamePassword';
 
 const Login = () => {
 
-
   return (
-    <section className='flex min-h-screen items-center justify-center cursor-default'>
-      <div className="border grid grid-cols-1 sm:grid-cols-2 border-blue-gray-500 rounded-2xl mx-10">
+    <div className="fade-in-left-container">
 
-        {/* Login Section Srart */}
-        <div className='items-center m-6 p-4 grid gap-1'>
+      <section className='flex min-h-screen items-center justify-center cursor-default'>
+        <div className="border grid grid-cols-1 sm:grid-cols-2 border-blue-gray-500 rounded-2xl mx-10">
 
-          {/* Responsive Logo & Title Section Start */}
-          <Grid
-            container
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            rowSpacing={1}
-            className='sm:hidden text-center mb-6'
-          >
-            <Grid item>
-              <Image
-                src={CompanyLogo}
-                width={64}
-                height={64}
-                alt="Company Logo"
-              />
+          {/* Login Section Srart */}
+          <div className='items-center m-6 p-4 grid gap-1'>
+
+            {/* Responsive Logo & Title Section Start */}
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              rowSpacing={1}
+              className='sm:hidden text-center mb-6'
+            >
+              <Grid item>
+                <Image
+                  src={CompanyLogo}
+                  width={64}
+                  height={64}
+                  alt="Company Logo"
+                  className='rotate-in-center'
+                />
+              </Grid>
+              <Grid item>
+                <Typography className='fade-in-left1 font-cinzel font-bold hidden 3xsm:block tracking-widest pl-2' variant='h4'>
+                  NT CORP
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography className='fade-in-left2 font-cinzel font-bold hidden 3xsm:block tracking-wide pl-2' variant='h6'>
+                  PT. BANGKITGIAT USAHA MANDIRI
+                </Typography>
+                <Typography className='fade-in-left3 font-cinzel font-bold hidden 3xsm:block tracking-wide pl-2 pb-7' variant='h6'>
+                  PALM OIL PLANTATION & MILL
+                </Typography>
+                <Divider variant="middle" />
+              </Grid>
             </Grid>
-            <Grid item>
-              <Typography className='font-cinzel font-bold hidden 3xsm:block tracking-widest pl-2' variant='h4'>
-                NT CORP
-              </Typography>
+            {/* Responsive Logo & Title Section End */}
+
+            <Typography className="fade-in-left4 sm:fade-in-left1 sm:text-left text-center font-extrabold" variant="h5">
+              NT CORP ANALYTICS&copy;
+            </Typography>
+            <Typography className="fade-in-left5 sm:fade-in-left2 sm:text-left text-center font-bold" variant="body1">
+              Simplify your digital analytics.
+            </Typography>
+            <Typography className="fade-in-left6 sm:fade-in-left3 sm:text-left text-center font-bolder text-blue-gray-500" variant="subtitle2">
+              Powered by JKO IT Development Team
+            </Typography>
+
+            <FormUsernamePassword />
+
+            <Tooltip title="Contact HRD via WhatsApp for further information.">
+              <Link className='text-right text-success text-sm' href="https://wa.me/+628170791244" target="_blank">
+                Forgot Password?
+              </Link>
+            </Tooltip>
+
+          </div>
+          {/* Login Section End */}
+
+          {/* Logo & Title Section Start */}
+          <div className='items-center m-6 p-4 text-center hidden sm:grid gap-0 duration-300 ease-in-out'>
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              rowSpacing={1}
+            >
+              <Grid item>
+                <Image
+                  src={CompanyLogo}
+                  width={64}
+                  height={64}
+                  alt="Company Logo"
+                  className='rotate-in-center'
+                />
+              </Grid>
+              <Grid item>
+                <Typography className='fade-in-left4 font-cinzel font-bold hidden 3xsm:block tracking-widest pl-2' variant='h4'>
+                  NT CORP
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography className='fade-in-left5 font-cinzel font-bold hidden 3xsm:block tracking-wide pl-2' variant='h6'>
+                  PT. BANGKITGIAT USAHA MANDIRI
+                </Typography>
+                <Typography className='fade-in-left6 font-cinzel font-bold hidden 3xsm:block tracking-wide pl-2' variant='h6'>
+                  PALM OIL PLANTATION & MILL
+                </Typography>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Typography className='font-cinzel font-bold hidden 3xsm:block tracking-wide pl-2' variant='h6'>
-                PT. BANGKITGIAT USAHA MANDIRI
-              </Typography>
-              <Typography className='font-cinzel font-bold hidden 3xsm:block tracking-wide pl-2 pb-7' variant='h6'>
-                PALM OIL PLANTATION & MILL
-              </Typography>
-              <Divider variant="middle" />
-            </Grid>
-          </Grid>
-          {/* Responsive Logo & Title Section End */}
-
-          <Typography className="sm:text-left text-center font-extrabold" variant="h5">
-            NT CORP ANALYTICS&copy;
-          </Typography>
-          <Typography className="sm:text-left text-center font-bold" variant="body1">
-            Simplify your digital analytics.
-          </Typography>
-          <Typography className="sm:text-left text-center font-bolder text-blue-gray-500" variant="subtitle2">
-            Powered by PT. BUM IT Development Team
-          </Typography>
-
-          <FormUsernamePassword />
-
-          <Tooltip title="Contact HRD via WhatsApp for further information.">
-            <Link className='text-right text-success text-sm' href="https://wa.me/+628170791244" target="_blank">
-              Forgot Password?
-            </Link>
-          </Tooltip>
+          </div>
+          {/* Logo & Title Section End */}
 
         </div>
-        {/* Login Section End */}
-
-        {/* Logo & Title Section Start */}
-        <div className='items-center m-6 p-4 text-center hidden sm:grid gap-0 duration-300 ease-in-out'>
-          <Grid
-            container
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            rowSpacing={1}
-          >
-            <Grid item>
-              <Image
-                src={CompanyLogo}
-                width={64}
-                height={64}
-                alt="Company Logo"
-              />
-            </Grid>
-            <Grid item>
-              <Typography className='font-cinzel font-bold hidden 3xsm:block tracking-widest pl-2' variant='h4'>
-                NT CORP
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography className='font-cinzel font-bold hidden 3xsm:block tracking-wide pl-2' variant='h6'>
-                PT. BANGKITGIAT USAHA MANDIRI
-              </Typography>
-              <Typography className='font-cinzel font-bold hidden 3xsm:block tracking-wide pl-2' variant='h6'>
-                PALM OIL PLANTATION & MILL
-              </Typography>
-            </Grid>
-          </Grid>
-        </div>
-        {/* Logo & Title Section End */}
-
-      </div>
-    </section>
+      </section>
+    </div>
   )
 }
 
