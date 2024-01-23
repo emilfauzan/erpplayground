99beae41b3e93808f5b4f8aea80e6acc8adeddfc
Emil Fauzan <emilfauzan@gmail.com>
2024-01-23 10:48:08 +0700

    return native id scroll

--- pages/dashboard/home/Home.tsx
+++ pages/dashboard/home/Home.tsx
@@ -10,19 +10,19 @@ import TableChartRoundedIcon from '@mui/icons-material/TableChartRounded';
 
 function Home() {
 
-    const handleScrollWithPadding = () => {
-        const targetElement = document.getElementById('#first-section');
+    // const handleScrollWithPadding = () => {
+    //     const targetElement = document.getElementById('#first-section');
 
-        if (targetElement) {
-            const paddingTop = 6; // Adjust this value based on your requirement
-            const scrollPosition = targetElement.offsetTop - paddingTop;
+    //     if (targetElement) {
+    //         const paddingTop = 6; // Adjust this value based on your requirement
+    //         const scrollPosition = targetElement.offsetTop - paddingTop;
 
-            window.scrollTo({
-                top: scrollPosition,
-                behavior: 'smooth',
-            });
-        }
-    };
+    //         window.scrollTo({
+    //             top: scrollPosition,
+    //             behavior: 'smooth',
+    //         });
+    //     }
+    // };
 
 
     return (
@@ -80,8 +80,8 @@ function Home() {
                     data-ga-event-category="docs"
                     data-ga-event-action="click-back-to-top"
                     aria-label="Scroll back to top"
-                    // href='#first-section'
-                    onClick={handleScrollWithPadding}
+                    href='#first-section'
+                    // onClick={handleScrollWithPadding}
                 >
                     <BadgeRoundedIcon />
                 </IconButton>
